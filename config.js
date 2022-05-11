49e8d2712d129d50ed5bea3f29c18893405516bf
Bhawa2007 <92066737+Bhawa2007@users.noreply.github.com>
2022-05-10 23:13:08 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA7mU1mjrK2laPKdmYARdqtiSiM1bY3VfM",
+  authDomain: "editing-1-b9dab.firebaseapp.com",
+  projectId: "editing-1-b9dab",
+  storageBucket: "editing-1-b9dab.appspot.com",
+  messagingSenderId: "1027994451661",
+  appId: "1:1027994451661:web:518f79974ddb2aa69e7384"
+};
+
